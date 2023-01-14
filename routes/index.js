dcbe40a24bdf26fcda59ff5b708151551276a481
suk-6 <admin@suk.kr>
2023-01-14 18:09:25 +0900

    feat: Update index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "DY-Card", title2: "DY-Card Backend" });
 });
 
 module.exports = router;
